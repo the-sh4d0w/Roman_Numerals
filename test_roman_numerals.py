f8119832fa548cdaab718b30081aafd42888692d
Benjamin <benjamin_gaertner@gmx.net>
2020-07-10 18:40:06 +0200

    Added more test cases

--- test_roman_numerals.py
+++ test_roman_numerals.py
@@ -10,6 +10,10 @@ class TestRomanNumerals(unittest.TestCase):
         self.assertEqual(main.roman_numerals_to_integer("MMXX"), 2020)
         self.assertEqual(main.roman_numerals_to_integer("MDCCLIII"), 1753)
         self.assertEqual(main.roman_numerals_to_integer("XCIX"), 99)
+        self.assertEqual(main.roman_numerals_to_integer("MCMXCIX"), 1999)
+        self.assertEqual(main.roman_numerals_to_integer("CCLI"), 251)
+        self.assertEqual(main.roman_numerals_to_integer("CMVII"), 907)
+        self.assertEqual(main.roman_numerals_to_integer("MMCCLV"), 2255)
 
     def test_integer_to_roman_numerals(self):
         self.assertEqual(main.integer_to_roman_numerals(1), "I")
@@ -17,6 +21,10 @@ class TestRomanNumerals(unittest.TestCase):
         self.assertEqual(main.integer_to_roman_numerals(2020), "MMXX")
         self.assertEqual(main.integer_to_roman_numerals(1753), "MDCCLIII")
         self.assertEqual(main.integer_to_roman_numerals(99), "XCIX")
+        self.assertEqual(main.integer_to_roman_numerals(1999), "MCMXCIX")
+        self.assertEqual(main.integer_to_roman_numerals(251), "CCLI")
+        self.assertEqual(main.integer_to_roman_numerals(907), "CMVII")
+        self.assertEqual(main.integer_to_roman_numerals(2255), "MMCCLV")
 
 
 if __name__ == "__main__":
