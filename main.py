a897b54ca698e70d849f4a603fb1fa314bd9c026
Benjamin <benjamin_gaertner@gmx.net>
2020-07-10 18:12:49 +0200

    Added integer to roman numerals translation

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 def roman_numerals_to_integer(roman_numerals: str) -> int:
     values = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900,
               "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    number = 0
+    integer = 0
     for i, j in values.items():
         for _ in range(roman_numerals.count(i)):
-            number += j
+            integer += j
         roman_numerals = roman_numerals.replace(i, "")
-    return number
+    return integer
 
 
 def integer_to_roman_numerals(integer: int) -> str:
-    pass
+    values = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC',
+              50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+    roman_numerals = ""
+    for i, j in values.items():
+        for _ in range(int(integer / i)):
+            roman_numerals += j
+            integer -= i
+    return roman_numerals
--- test_roman_numerals.py
+++ test_roman_numerals.py
@@ -8,11 +8,15 @@ class TestRomanNumerals(unittest.TestCase):
         self.assertEqual(main.roman_numerals_to_integer("I"), 1)
         self.assertEqual(main.roman_numerals_to_integer("XIV"), 14)
         self.assertEqual(main.roman_numerals_to_integer("MMXX"), 2020)
+        self.assertEqual(main.roman_numerals_to_integer("MDCCLIII"), 1753)
+        self.assertEqual(main.roman_numerals_to_integer("XCIX"), 99)
 
     def test_integer_to_roman_numerals(self):
         self.assertEqual(main.integer_to_roman_numerals(1), "I")
         self.assertEqual(main.integer_to_roman_numerals(14), "XIV")
         self.assertEqual(main.integer_to_roman_numerals(2020), "MMXX")
+        self.assertEqual(main.integer_to_roman_numerals(1753), "MDCCLIII")
+        self.assertEqual(main.integer_to_roman_numerals(99), "XCIX")
 
 
 if __name__ == "__main__":
